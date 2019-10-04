9b0b63866b917658c6b73b614b18f78262666226
Emmanuel Owojori <34447078+phiiileo@users.noreply.github.com>
2019-10-04 01:33:11 +0100

    Added "config.php" file for the google signin
    This file needs editing for it to function properly on your local machine. Read the comments embedded in the codes to know what to do.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+    session_start();
+    require_once "GoogleAPIUpdated/vendor/autoload.php";
+    $gclient = new Google_Client();
+    $gclient -> setClientId("267104905609-64v7asrtbclruu33c6vprrrkfe6p9n3d.apps.googleusercontent.com"); #change this to the new one you generated
+    $gclient -> setClientSecret("0YkFkrsuHkHEgr8goKQdnYt5"); #This one also
+    $gclient -> setApplicationName("STYX-FINANCE-TRACKER-APP"); #Give your app this name "STYX-FINANCE-TRACKER-APP"
+    $gclient -> setRedirectUri("http://localhost/hngi/team_STYX-FinancialTracker/gcallback.php"); #TO WHO TO HOST/forking this codes: CREATE GOOGLE CREDENTIAL FOR THIS APP USING THIS LINK = https://console.cloud.google.com/apis/credentials/oauthclient/ without this, the app wont open.
+    $gclient -> addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
+?>
