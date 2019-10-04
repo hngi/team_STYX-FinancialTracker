e8a8dc24827e9ca4ae860ffefdd85f274d2cfd04
Emmanuel Owojori <34447078+phiiileo@users.noreply.github.com>
2019-10-04 01:45:19 +0100

    modified to sign out from google account

--- logout.php
+++ logout.php
@@ -3,6 +3,13 @@
 	include("includes/config.php");
 	$_SESSION['id'] == "";
 	
+	require_once "config.php";
+    	unset($_SESSION['access_token']);
+    	$gclient -> revokeToken();
+    	session_destroy();
+    	header("location : login.php");
+    	exit();
+
 	session_unset();
 	session_destroy();
 	header('location:login.php');
