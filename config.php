a90944ffed1309a4e6739eda5691005da4d0b788
Emmanuel Owojori <34447078+phiiileo@users.noreply.github.com>
2019-10-04 01:53:05 +0100

    updated the signIn to work with the hosted space

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
     $gclient -> setClientId("267104905609-64v7asrtbclruu33c6vprrrkfe6p9n3d.apps.googleusercontent.com"); #change this to the new one you generated
     $gclient -> setClientSecret("0YkFkrsuHkHEgr8goKQdnYt5"); #This one also
     $gclient -> setApplicationName("STYX-FINANCE-TRACKER-APP"); #Give your app this name "STYX-FINANCE-TRACKER-APP"
-    $gclient -> setRedirectUri("http://localhost/hngi/team_STYX-FinancialTracker/gcallback.php"); #TO WHO TO HOST/forking this codes: CREATE GOOGLE CREDENTIAL FOR THIS APP USING THIS LINK = https://console.cloud.google.com/apis/credentials/oauthclient/ without this, the app wont open.
+    $gclient -> setRedirectUri("http://styx.afroblogit.com/financetracker/gcallback.php"); #TO WHO TO HOST: correct this to the appropriate. if forking this codes: CREATE GOOGLE CREDENTIAL FOR THIS APP USING THIS LINK = https://console.cloud.google.com/apis/credentials/oauthclient/ without this, the app wont open.
     $gclient -> addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
