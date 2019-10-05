225704a917d39814776770bb23e16f3697b391f7
tosyno123 <oluwatomisin.owolabi@yahoo.com>
2019-10-05 17:15:31 +0100

    updated files (#19)

--- index.php
+++ index.php
@@ -1,252 +1,192 @@
-<?php 
-  ob_start();
-  include('includes/config.php'); 
-?>
-
 <!DOCTYPE html>
+
 <html lang="en">
 
+
+
 <head>
+
   <meta charset="utf-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>STYX Finance Tracker</title>
+
+  <title>STYX Finance Tracker || Home</title>
+
+  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
-  
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/css/mdb.min.css" rel="stylesheet">
+
+
+
   <style type="text/css">
+
     html,
+
     body,
+
     header,
+
     .view {
+
       height: 100%;
-    }
-     .btn-indigo {
-         color: #fff;
-         background: #3F51B5!important;
-        transform: translate(-50%,-50%)!important;
-/*         box-shadow: 0 20px 50px rgba(0,0,0,0.3)!important;*/
-         overflow: hidden!important;
-         border-radius: 0px!important;
-         transition: 0.2s!important;
-    }
-      .btn-indigo:hover {
-/*          color: aq;*/
-          background: #2196f3!important;
-         box-shadow: 0 0 5px #2196f3, 0 0 20px #2196f3, 0 0 40px #2196f3!important;
-         border-radius: 0px!important;
-          transition-delay: .1s!important;
-    }
-      .btn-indigo span {
-          position: absolute;
-          display: block;
-      }
-       .btn-indigo span:nth-child(1) {
-          top: 0;
-          left: -100%;
-          width: 100%;
-          height: 2px;
-          background: linear-gradient(90deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(1) {
-          left: 100%;
-          transition: 1s;
-      }
-      
-      .btn-indigo span:nth-child(3) {
-          bottom: 0;
-          right: -100%;
-          width: 100%;
-          height: 2px;
-          background: linear-gradient(270deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(3) {
-          right: 100%;
-          transition: 1s;
-          transition-delay: 0.5s;
-      }
-      
-      .btn-indigo span:nth-child(2) {
-          top: -100%;
-          right: 0;
-          width: 2px;
-          height: 100%;
-          background: linear-gradient(180deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(2) {
-          top: 100%;
-          transition: 1s;
-          transition-delay: 0.25s;
-      }
-      
-      .btn-indigo span:nth-child(4) {
-          bottom: -100%;
-          left: 0;
-          width: 2px;
-          height: 100%;
-          background: linear-gradient(360deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(4) {
-          bottom: 100%;
-          transition: 1s;
-          transition-delay: 0.75s;
-      }
 
-    @media (max-width: 768px) {
+    }
+
+
+
+    @media (max-width: 740px) {
+
       html,
+
       body,
+
       header,
+
       .view {
-        height: 1000px;
-      }
 
-      .navbar {
-        background-color: #1C2331; 
-      }
+        height: 100%;
 
-      .title {
-        font-size: 40px;
-        margin-top: 50px;
-      }
-
-      .login-form {
-        margin-top: -e50px;
-      }
-    
-    }
-
-    @media (min-width: 768px) {
-      .title {
-        font-size: 50px;
-        margin-top: 100px;
       }
 
     }
 
-    .top-nav-collapse {
-      background-color: #1C2331; 
+
+
+    .btn {
+
+      background-color: #008D1F;
+
+      -webkit-box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
+
+      box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
+
     }
 
-    .navbar {
-      box-shadow: none;
+
+
+    .btn:hover {
+
+      background-color: #008D1F;
+
+      color: #fff;
+
     }
 
-    .nav-link {
-      font-weight: bold;
-      font-size: 18px;
-    }
 
-    .nav-item {
-      padding-right: 30px;
-    }
 
   </style>
+
 </head>
+
+
 
 <body>
 
-  <?php include('includes/navbar.php'); ?>
 
 
-  <div class="view full-page-intro" style="background-image: url('https://res.cloudinary.com/phiileo/image/upload/v1570269444/landing_kav9zp.jpg'); background-repeat: no-repeat; background-size: cover;">
+  <div class="view full-page-intro" style="background-image: url('images/landing.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+
 
     <div class="mask rgba-blue-light d-flex justify-content-center align-items-center">
 
-      <div class="container">
-        <div class="row wow fadeIn">
-          <div class="col-md-7 mb-4 white-text text-center text-md-left">
 
-            <h1 class="title font-weight-bold">STYX Finance Tracker</h1>
+
+      <div class="container">
+
+
+
+        <div class="row wow fadeIn">
+
+
+
+          <div class="col-md-7 mb-4 white-text text-md-left">
+
+
+
+            <h1 class="display-4 font-weight-bold">STYX Finance Tracker</h1>
+
+
 
             <hr class="hr-light">
 
-            <p class="h5 d-none d-md-block">
+
+
+            <h5>
+
               <strong>Be in charge of your money</strong>
-            </p>
-            <p class="mb-4 h5 d-none d-md-block">
-              <strong>Create a new Styx account and take charge</strong>
-            </p>
+
+            </h5>
+
+
+
+            <h5 class="mb-4 ">
+
+              <strong>Create a new Styx finance account and take charge</strong>
+
+            </h5>
+
+
+
+            <a href="login.php" class="btn btn-lg">Login
+
+              <i class="fas fa-sign-in-alt ml-2"></i>
+
+            </a>
+
+
 
           </div>
 
-          <div class="col-md-6 col-xl-5 mb-4 mt-5">
-            <div class="card mb-5 login-form">
-              <div class="card-body">
-                <form name="" action="includes/authentication.php" method="post">
 
-                  <h3 class="dark-grey-text text-center">
-                    <strong>LOGIN</strong>
-                  </h3>
-                  <hr>
 
-                  <?php 
-                    if (isset($_SESSION['success'])) {
-                        echo "<div class='alert alert-success'>" .$_SESSION['success']. "</div>";
-                        session_unset();
-                    } 
-                    
-                    if (isset($_SESSION['invalid'])) {
-                        echo "<div class='alert alert-danger'>" .$_SESSION['invalid']. "</div>";
-                        session_unset();
-                    }
+          <div class="col-md-3"></div>
 
-                  ?>
 
-                  <div class="md-form">
-                    <i class="fas fa-envelope prefix grey-text"></i>
-                    <input type="text" name="email" class="form-control">
-                    <label for="form2">Email</label>
-                  </div>
-                  
-                  <div class="md-form">
-                    <i class="fas fa-lock prefix grey-text"></i>
-                    <input type="password" name="password" class="form-control">
-                    <label for="form2">Password</label>
-                  </div>
-                  
-                  <div class="text-center">
-                    <a href="forgot_password.php">Forgot Password</a>
-                    <button class="btn btn-indigo" type="submit" name="login">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    SIGN IN</button>
-                    <hr>
-                    <p class="mt-5">Don't have an Account? <a href="signup.php">Sign Up</a></p>
-                  </div>
-
-                </form>
-
-              </div>
-
-            </div>
-
-          </div>
 
         </div>
 
+
+
       </div>
+
+
 
     </div>
 
+
+
   </div>
-  
+
+
+
+
+
+  <!-- JQuery -->
 
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
   <!-- Bootstrap tooltips -->
+
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+
   <!-- Bootstrap core JavaScript -->
+
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
-  <!-- Initializations -->
-  <script type="text/javascript">
-    // Animations initialization
-    new WOW().init();
-  </script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/js/mdb.min.js"></script>
+
 </body>
 
+
+
 </html>
+
--- signup.php
+++ signup.php
@@ -1,284 +1,159 @@
-<?php 
-  ob_start();
-  include('includes/config.php'); 
-?>
-
 <!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>STYX Finance Tracker</title>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<html>
 
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
-  
-  <style type="text/css">
-    html,
-    body,
-    header,
-    .view {
-      height: 100%;
-    }
-     .btn-indigo {
-         color: #fff;
-         background: #3F51B5!important;
-        transform: translate(-50%,-50%)!important;
-/*         box-shadow: 0 20px 50px rgba(0,0,0,0.3)!important;*/
-         overflow: hidden!important;
-         border-radius: 0px!important;
-         transition: 0.2s!important;
-    }
-      .btn-indigo:hover {
-/*          color: aq;*/
-          background: #2196f3!important;
-         box-shadow: 0 0 5px #2196f3, 0 0 20px #2196f3, 0 0 40px #2196f3!important;
-         border-radius: 0px!important;
-          transition-delay: .1s!important;
-    }
-      .btn-indigo span {
-          position: absolute;
-          display: block;
-      }
-       .btn-indigo span:nth-child(1) {
-          top: 0;
-          left: -100%;
-          width: 100%;
-          height: 2px;
-          background: linear-gradient(90deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(1) {
-          left: 100%;
-          transition: 1s;
-      }
-      
-      .btn-indigo span:nth-child(3) {
-          bottom: 0;
-          right: -100%;
-          width: 100%;
-          height: 2px;
-          background: linear-gradient(270deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(3) {
-          right: 100%;
-          transition: 1s;
-          transition-delay: 0.5s;
-      }
-      
-      .btn-indigo span:nth-child(2) {
-          top: -100%;
-          right: 0;
-          width: 2px;
-          height: 100%;
-          background: linear-gradient(180deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(2) {
-          top: 100%;
-          transition: 1s;
-          transition-delay: 0.25s;
-      }
-      
-      .btn-indigo span:nth-child(4) {
-          bottom: -100%;
-          left: 0;
-          width: 2px;
-          height: 100%;
-          background: linear-gradient(360deg, transparent, #fff);
-      }
-      .btn-indigo:hover span:nth-child(4) {
-          bottom: 100%;
-          transition: 1s;
-          transition-delay: 0.75s;
-      }
-    @media (max-width: 768px) {
-      html,
-      body,
-      header,
-      .view {
-        height: 1000px;
-      }
+    <head>
 
-      .navbar {
-        background-color: #1C2331; 
-      }
+        <title>STYX Finance Tracker || Sign Up</title>
 
-      .title {
-        font-size: 40px;
-        margin-top: 50px;
-      }
+        <meta charset="UTF-8">
 
-      .register-form {
-        margin-top: -50px;
-      }
-    
-    }
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @media (min-width: 768px) {
-      .title {
-        font-size: 50px;
-        margin-top: 150px;
-      }
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-      .register-form {
-        margin-top: 50px;
-      }
+        <link rel="stylesheet" href="main.css">
 
-    }
+    </head>
 
-    .top-nav-collapse {
-      background-color: #1C2331; 
-    }
+    <body>
 
-    .navbar {
-      box-shadow: none;
-    }
+        <div class="row">
 
-    .nav-link {
-      font-weight: bold;
-      font-size: 18px;
-    }
+            <div class="vec col-md-6">
 
-    .nav-item {
-      padding-right: 30px;
-    }
+                <h5 class="text-center">Be in charge of your money</h5>
 
-  </style>
-</head>
+                <p class="text-center">Create a new Styx finance account and take charge</p>
 
-<body>
-
-  <?php include('includes/navbar.php'); ?>
-
-
-  <div class="view full-page-intro" style="background-image: url('landing.jpg'); background-repeat: no-repeat; background-size: cover;">
-
-    <div class="mask rgba-blue-light d-flex justify-content-center align-items-center">
-
-      <div class="container">
-        <div class="row wow fadeIn">
-          <div class="col-md-7 mb-4 white-text text-center text-md-left">
-
-            <h1 class="title font-weight-bold">STYX Finance Tracker</h1>
-
-            <hr class="hr-light">
-
-            <p class="h5 d-none d-md-block">
-              <strong>Be in charge of your money</strong>
-            </p>
-            <p class="mb-4 h5 d-none d-md-block">
-              <strong>Create a new Styx account and take charge</strong>
-            </p>
-
-          </div>
-
-          <div class="col-md-6 col-xl-5 mb-4 mt-5">
-            <div class="card mb-5 register-form">
-              <div class="card-body">
-                <form name="" action="includes/authentication.php" method="post">
-
-                  <h3 class="dark-grey-text text-center">
-                    <strong>CREATE ACCOUNT</strong>
-                  </h3>
-                  <hr>
-
-                  <?php
-                    include("includes/authentication.php");
-                    
-                    if(isset($_SESSION['errors']['empty'])){
-                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['empty']. "</div>";
-                        unset($_SESSION['errors']['empty']);
-                    }
-                    
-                    if(isset($_SESSION['errors']['password'])){
-                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['password']. "</div>";
-                        unset($_SESSION['errors']['password']);
-                    }
-                    
-                    if(isset($_SESSION['errors']['number'])){
-                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['number']. "</div>";
-                        unset($_SESSION['errors']['number']);
-                    }
-                    
-                    if(isset($_SESSION['errors']['email'])){
-                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['email']. "</div>";
-                        unset($_SESSION['errors']['email']);
-                    }
-                    
-                    if(isset($_SESSION['errors']['fname'])){
-                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['fname']. "</div>";
-                        unset($_SESSION['errors']['fname']);
-                    }
-
-                  ?>
-
-                  <div class="md-form">
-                    <i class="fas fa-user prefix grey-text"></i>
-                    <input type="text" name="fname" class="form-control">
-                    <label for="form2">Full Name</label>
-                  </div>
-
-                  <div class="md-form">
-                    <i class="fas fa-envelope prefix grey-text"></i>
-                    <input type="text" name="email" class="form-control">
-                    <label for="form2">Email</label>
-                  </div>
-
-                  <div class="md-form">
-                    <i class="fas fa-phone prefix grey-text"></i>
-                    <input type="text" name="phone" class="form-control">
-                    <label for="form2">Phone Number</label>
-                  </div>
-                  
-                  <div class="md-form">
-                    <i class="fas fa-lock prefix grey-text"></i>
-                    <input type="password" name="pword" class="form-control">
-                    <label for="form2">Password</label>
-                  </div>
-
-                  <div class="md-form">
-                    <i class="fas fa-lock prefix grey-text"></i>
-                    <input type="password" name="cpword" class="form-control">
-                    <label for="form2">Confirm Password</label>
-                  </div>
-                  
-                  <div class="text-center">
-                    <button class="btn btn-indigo" type="submit" name="register">SIGN UP</button>
-                    <hr>
-                    <p>Already have an account? <a href="index.php">Sign In</a></p>
-                  </div>
-
-                </form>
-
-              </div>
+                <img src="images/background.png">
 
             </div>
 
-          </div>
+            
 
-        </div>
+            <!--form-->
 
-      </div>
+            <div class="col-12 col-sm-12 col-md-6 main">
 
-    </div>
+                <?php
 
-  </div>
-  
+                    include("includes/authentication.php");
 
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
-  <!-- Initializations -->
-  <script type="text/javascript">
-    // Animations initialization
-    new WOW().init();
-  </script>
-</body>
+                    if(isset($_SESSION['errors']['empty'])){
+
+                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['empty']. "</div>";
+
+                        unset($_SESSION['errors']['empty']);
+
+                    }
+
+                    if(isset($_SESSION['errors']['password'])){
+
+                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['password']. "</div>";
+
+                        unset($_SESSION['errors']['password']);
+
+                    }
+
+                    if(isset($_SESSION['errors']['number'])){
+
+                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['number']. "</div>";
+
+                        unset($_SESSION['errors']['number']);
+
+                    }
+
+                    if(isset($_SESSION['errors']['email'])){
+
+                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['email']. "</div>";
+
+                        unset($_SESSION['errors']['email']);
+
+                    }
+
+                    if(isset($_SESSION['errors']['pass_leng'])){
+
+                        echo "<div class='alert alert-danger'>" .$_SESSION['errors']['pass_leng']. "</div>";
+
+                        unset($_SESSION['errors']['pass_leng']);
+
+                    }
+
+                ?>
+
+                <h4 class="abs">Sign Up</h4>
+
+              <form method="post" action="includes/authentication.php">
+
+                
+
+                <div class="form-group mb-4">
+
+                    <label for="name">Full Name:</label> 
+
+                    <input type="text" class="form-control" name="fname">
+
+                </div>
+
+                <div class="form-group mb-4">
+
+                    <label for="email">Email address:</label> 
+
+                    <input type="email" class="form-control" name="email">
+
+                </div>
+
+                <div class="form-group mb-4">
+
+                    <label for="number">Phone Number:</label> 
+
+                    <input type="text" class="form-control" name="phone">
+
+                </div>
+
+                <div class="form-group mb-4">
+
+                    <label for="password">Password:</label>
+
+                    <input type="password" class="form-control" name="pword">
+
+                </div>
+
+                <div class="form-group">
+
+                    <label for="password">Confirm Password:</label>
+
+                    <input type="password" class="form-control" name="cpword">
+
+                </div>
+
+                <br>
+
+                <!--sign up button-->
+
+                <input type="submit" class="button" name="register" value="Sign Up">
+
+            </form>
+
+            <br>    
+
+            <!-- sign in link-->
+
+            <p class="text-center">Already have an account? <a href="login.php" class="signUpLink">Sign In</a></p>
+
+           </div>
+
+        </div> 
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    </body>
 
 </html>
